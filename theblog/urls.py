1bcd508b75bffb287aec16efaf97e0146889ce15
Patrick Guo <guopatrick.courses@gmail.com>
2020-11-10 18:42:02 -0800

    y

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 from .views import Home
 
 urlpatterns = [
@@ -17,4 +18,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home")
 >>>>>>> parent of 01ff698... 2
+=======
+from .views import HomeView
+
+urlpatterns = [
+    path('', HomeView,as_view(), name="home"),
+>>>>>>> parent of 471f4b3... homeview
 ]

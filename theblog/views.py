0756a7e76912eac498500de14c8196d793be720b
Patrick Guo <guopatrick.courses@gmail.com>
2020-11-11 21:35:37 -0800

    Blog Category Pages - Django Blog #13

--- theblog/views.py
+++ theblog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -8,6 +8,10 @@ class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['-post_date']
+
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats)
+    return render(request, 'categories.html', {'cats': cats, 'category_posts': category_posts})
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -27,3 +31,8 @@ class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
+
+class AddCategoryView(CreateView):
+    model = Category
+    form_class = '__all__'
+    template_name = 'add_category.html'

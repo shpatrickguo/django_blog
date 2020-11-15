bc9d6a55988184f705e50bf5bcd06a50abda1298
Patrick Guo <guopatrick.courses@gmail.com>
2020-11-14 22:33:17 -0800

    Edit Profile Page Form - Django Blog #31

--- members/views.py
+++ members/views.py
@@ -7,6 +7,12 @@ from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
 from theblog.models import Profile
 
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'linkedin_url']
+    success_url = reverse_lazy('home')
+
 class ShowProfilePageView(DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'

8cfc9ddb21448432b2eb8f4cae38fe0a48478290
Patrick Guo <guopatrick.courses@gmail.com>
2020-11-22 19:37:23 -0800

    sales csv upload

--- csvs/views.py
+++ csvs/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
-from djagno.http import HttpResponse
+from .forms import CsvModelForm
+from .models import Csv
+import csv
+from django.contrib.auth.models import User
+from sales.models import Sale
 
-# Create your views here.
 def upload_file_view(request):
-    return HttpResponse('drop a file here')
+    form = CsvModelForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        form = CsvModelForm()
+        obj = Csv.objects.get(activated=False)
+        with open(obj.file_name.path, 'r') as f:
+            reader = csv. reader(f)
+
+            for i, row in enumerate(reader):
+                if i == 0:
+                    pass
+                else:
+                    row = "".join(row)
+                    row = row.replace*(";", "")
+                    row = row.split()
+                    product = row[1].upper()
+                    user = User.objects.get(username=row[3])
+                    Sale.objects.create(
+                        product = product,
+                        quantity = int(row[2]),
+                        salesman = user, 
+                    )
+            obj.activated = True
+            obj.save()
+    return render(request, 'csvs/upload.html', {'form': form})

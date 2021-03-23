812e7bcf0010543ef2120021087664b5cae9a378
Patrick Guo <guopatrick.courses@gmail.com>
2021-03-22 23:51:20 -0700

    environ

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
--- security/models.py
+++ security/models.py
@@ -1,8 +1,20 @@
+from random import choice
+
+from twilio.rest import Client
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ActivityLog(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     action = models.CharField(max_length=300)
 
+class TwoFactorAuthentification(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    code = models.CharField(max_length=6)
+    sent_on = models.DateTimeField(auto_now_add=True)
 
+    @classmethod
+    def send_code(cls, user, to_phone):
+        account_sid = "AC5a082b2b57a8b32da3373225b7a80677"
